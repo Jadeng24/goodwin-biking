cbee6aa95a7074433c611789d5994a420cf83d96
jaden <jaden.goodwin24@gmail.com>
2024-04-30 16:26:42 -0600

    adding router and redux state to app with pages and nav

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.scss";
 import App from "./App";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { theme } from "./theme";
+
+import "./index.scss";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+
+import cartReducer from "./state";
+
+const store = configureStore({
+  reducer: { cart: cartReducer },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
