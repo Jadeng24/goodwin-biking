b3e3f060a6a4c7b71cd9e16e1caa6902557a3941
jaden <jaden.goodwin24@gmail.com>
2024-05-06 00:57:32 -0600

    Adding empty cart message and icon

--- client/src/pages/global/cart-menu/cart-drawer/CartDrawer.tsx
+++ client/src/pages/global/cart-menu/cart-drawer/CartDrawer.tsx
@@ -1,16 +1,21 @@
 import { useDispatch, useSelector } from "react-redux";
 
-import { Close } from "@mui/icons-material";
+import {
+  Close,
+  HourglassEmpty,
+  ProductionQuantityLimits,
+} from "@mui/icons-material";
 import { Box, IconButton, Typography, useMediaQuery } from "@mui/material";
 
 import { CartActions } from "./cart-actions";
 import { CartItems } from "./cart-items";
 import { Flex } from "../../../../components";
 import { RootState, setIsCartOpen } from "../../../../redux-store";
+import { shades } from "../../../../theme";
 
 export const CartDrawer = () => {
   const dispatch = useDispatch();
-  const cart = useSelector((state: RootState) => state.cart.cartItems);
+  const cartItems = useSelector((state: RootState) => state.cart.cartItems);
 
   const isGreaterThanMobile = useMediaQuery("(min-width:600px)");
 
@@ -27,15 +32,35 @@ export const CartDrawer = () => {
     >
       <Box padding="30px" overflow="auto" height="100%">
         <Flex marginBottom="15px" justifyContent="space-between">
-          <Typography variant="h3">SHOPPING BAG ({cart.length})</Typography>
+          <Typography variant="h3">
+            SHOPPING BAG ({cartItems.length})
+          </Typography>
           <IconButton onClick={() => dispatch(setIsCartOpen({}))}>
             <Close />
           </IconButton>
         </Flex>
-
-        <CartItems />
-
-        <CartActions />
+        {cartItems && cartItems?.length > 0 ? (
+          <>
+            <CartItems />
+            <CartActions />
+          </>
+        ) : (
+          <Flex flexDirection="column">
+            <Typography
+              variant="h3"
+              marginBottom="120px"
+              sx={{ maxWidth: "90%" }}
+            >
+              Oops! It looks like you don't have any products in your cart yet.
+            </Typography>
+            <Flex width="100%" alignItems="center" justifyContent="center">
+              <ProductionQuantityLimits
+                fontSize="large"
+                sx={{ transform: "scale(2.5)" }}
+              />
+            </Flex>
+          </Flex>
+        )}
       </Box>
     </Box>
   );
