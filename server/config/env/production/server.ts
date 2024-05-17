91ddd44356d030876437592636bba155864c22ca
jaden <jaden.goodwin24@gmail.com>
2024-05-16 19:05:48 -0600

    removing unneeded port and host in prod server.ts

--- server/config/env/production/server.ts
+++ server/config/env/production/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   proxy: true,
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
+  //   host: env("HOST", "0.0.0.0"),
+  //   port: env.int("PORT", 1337),
   url: env("APP_URL"), // Sets the public URL of the application.
   app: {
     keys: env.array("APP_KEYS"),
