ec401f7bebe9ceb640c6206ff4bb7c92e7de5d22
jaden <jaden.goodwin24@gmail.com>
2024-05-16 21:01:14 -0600

    adding /api to url

--- server/config/env/production/server.ts
+++ server/config/env/production/server.ts
@@ -1,8 +1,8 @@
 export default ({ env }) => ({
   proxy: true,
-  //   host: env("HOST", "0.0.0.0"),
-  //   port: env.int("PORT", 1337),
-  url: env("APP_URL"), // Sets the public URL of the application.
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: `${env("APP_URL")}/api`, // Sets the public URL of the application.
   app: {
     keys: env.array("APP_KEYS"),
   },
