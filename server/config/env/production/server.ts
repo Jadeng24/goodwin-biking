ee12081a27f9d3def15ae529b50cb1d796c2054b
jaden <jaden.goodwin24@gmail.com>
2024-05-16 21:04:07 -0600

    fixing url

--- server/config/env/production/server.ts
+++ server/config/env/production/server.ts
@@ -2,7 +2,7 @@ export default ({ env }) => ({
   proxy: true,
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: `${env("APP_URL")}/api`, // Sets the public URL of the application.
+  url: env("APP_URL"), // Sets the public URL of the application.
   app: {
     keys: env.array("APP_KEYS"),
   },
