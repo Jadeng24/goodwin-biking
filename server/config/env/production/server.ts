806d0d9a01620e6ffdc5800e9b4bcfaba5742316
jaden <jaden.goodwin24@gmail.com>
2024-05-16 21:12:02 -0600

    removing url

--- server/config/env/production/server.ts
+++ server/config/env/production/server.ts
@@ -1,8 +1,8 @@
 export default ({ env }) => ({
-  proxy: true,
+  //   proxy: true,
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("APP_URL"), // Sets the public URL of the application.
+  url: env("APP_URL"), // Sets the public URL of the application, Do not change
   app: {
     keys: env.array("APP_KEYS"),
   },
