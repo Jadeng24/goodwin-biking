ffcc1669e3b73b269888b76431f593a2348e6d73
jaden <jaden.goodwin24@gmail.com>
2024-05-18 12:45:41 -0600

    fixing host default

--- server/config/env/production/server.ts
+++ server/config/env/production/server.ts
@@ -1,6 +1,6 @@
 export default ({ env }) => ({
   proxy: true,
-  host: env("HOST", "www.goodwinbiking.com/"),
+  host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   url: env("APP_URL"), // Sets the public URL of the application, Do not change
   app: {
