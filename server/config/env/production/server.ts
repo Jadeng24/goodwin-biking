21b6d6afdb9499bbab2adfedf744eaa2de6ebc78
jaden <jaden.goodwin24@gmail.com>
2024-05-18 12:22:15 -0600

    adding change to env

--- server/config/env/production/server.ts
+++ server/config/env/production/server.ts
@@ -3,7 +3,6 @@ export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   url: env("APP_URL"), // Sets the public URL of the application, Do not change
-  //   url: env("PUBLIC_URL" , "https://www.goodwinbiking.com/api"),
   app: {
     keys: env.array("APP_KEYS"),
   },
