d219f68c8478bfadaacbabc729a3e8375924bdef
jaden <jaden.goodwin24@gmail.com>
2024-05-18 11:58:00 -0600

    removing backup url

--- server/config/env/production/server.ts
+++ server/config/env/production/server.ts
@@ -2,7 +2,7 @@ export default ({ env }) => ({
   proxy: true,
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("APP_URL", "https://www.goodwinbiking.com/api"), // Sets the public URL of the application, Do not change
+  url: env("APP_URL"), // Sets the public URL of the application, Do not change
   //   url: env("PUBLIC_URL" , "https://www.goodwinbiking.com/api"),
   app: {
     keys: env.array("APP_KEYS"),
