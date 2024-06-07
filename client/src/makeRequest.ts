9ef8c5a1d48386da3b855d1f18af1bffbbceda2c
jaden <jaden.goodwin24@gmail.com>
2024-06-07 00:11:46 -0600

    switching to base url for hosted site

--- client/src/makeRequest.ts
+++ client/src/makeRequest.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
-import { API_URL } from "./environment";
+import { BASE_URL } from "./environment";
 // Uses the api url provided in .env
 // for example: http://localhost:1337/api/orders or https://www.goodwinbiking.com/api
 export const makeRequest = axios.create({
-  baseURL: API_URL,
+  baseURL: BASE_URL, // for Local testing, use API_URL
   headers: {
     Authorization: "bearer " + process.env.REACT_APP_STRAPI_TOKEN,
   },
